469c017cae0410d654cff12d5d5378fde0ed0d3b
maratkhanovna <maratkhanovna@gmail.com>
2022-08-27 12:28:55 -0400

    Solar system ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         //write your code here
+        SolarSystem inception = new SolarSystem();
+        inception.sun = 1;
+        inception.moon = 1;
+        inception.planets = 2;
 
+        System.out.println("the basic structure is ready");
     }
 }
